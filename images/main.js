2c736809192f9c40e4a39beb20b33bba59772830
Adrian Gonzalez <adrian131@me.com>
2020-11-05 16:38:50 -0800

    Created keydown listener and able to turn car with corresponding arrow key pressed.

--- images/main.js
+++ images/main.js
@@ -1,31 +1,24 @@
+var racecarImg = document.querySelector('.racecar');
+
 var car = {
 direction: ''
-
 }
 
-var racecarImg = document.querySelector('.racecar')
 
 
+// listens for keydown event, turns car in arrow direction, and updates data model
 window.addEventListener('keydown', function() {
   if (event.key === 'ArrowLeft') {
     racecarImg.className = 'racecar west';
     car.direction = 'west';
-    this.console.log(event.key)
-    this.console.log(car)
   } else if (this.event.key === 'ArrowRight') {
     racecarImg.className = 'racecar';
     car.direction = 'east';
-    this.console.log(event.key)
-    this.console.log(car)
   } else if (this.event.key === 'ArrowUp') {
     racecarImg.className = 'racecar north';
     car.direction = 'north';
-    this.console.log(event.key)
-    this.console.log(car)
   } else if (this.event.key === 'ArrowDown') {
     racecarImg.className = 'racecar south';
     car.direction = 'south';
-    this.console.log(event.key)
-    this.console.log(car)
   }
 })
